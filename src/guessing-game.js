6419d2a8efe1c64873d9411e6459287883cc0c63
Dziazhurny <grafton999@gmail.com>
2019-10-09 20:39:34 +0300

    feat: solve task

---
+++ src/guessing-game.js
@@ -0,0 +1,25 @@
+class GuessingGame {
+    constructor() {
+               
+      }
+    
+      setRange(min, max) {
+        this.min=min;
+        this.max=max;
+      }
+    
+      guess() {
+        this.guessing=Math.round((this.min+this.max)/2);
+        return this.guessing;
+      }
+    
+      lower() {
+          this.max=this.guessing;
+      }
+    
+      greater() {
+          this.min=this.guessing;
+      }
+}
+
+module.exports = GuessingGame;
